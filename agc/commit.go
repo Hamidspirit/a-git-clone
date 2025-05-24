b541e640627f501f4252a48ad8d3b88f3a4e214e
Spirit <hamidrezacheghini578@gmail.com>
2025-05-24 20:54:40 +0330

    add commit and head

--- agc/commit.go
+++ agc/commit.go
@@ -1,5 +1,30 @@
 package agc
 
-func (g *GitStruct) Commit(msg string) {
+import (
+	"crypto/sha1"
+	"fmt"
+	"log"
+	"os"
+)
 
+func Commit(msg string) {
+	treehash := WriteTree()
+	commit := fmt.Sprintf("tree %s\n\n%s", treehash, msg)
+	commithash := fmt.Sprintf("%x", sha1.Sum([]byte(commit)))
+	fmt.Printf("commit hash: %s\ncommit:\n\t %s", commithash, commit)
+
+	err := SaveCommitObj(commit)
+	if err != nil {
+		log.Fatal("Failed to create commit object")
+	}
+
+}
+
+func SaveCommitObj(data string) error {
+	file, err := os.Create(GitRepo + "/Head")
+	if err != nil {
+		return err
+	}
+	file.Write([]byte(data))
+	return nil
 }
